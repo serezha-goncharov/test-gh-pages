f2eca84f047fd0b95a953d549d29f8cb84e3e23d
Sergei Goncharov <blist.zheleza@gmail.com>
2024-11-03 22:36:10 +0700

    new deploy to gh-pages

---
+++ task-name/assets/index-BWDW8IAO.js
@@ -0,0 +1,2 @@
+import"./modulepreload-polyfill-B5Qt9EMX.js";console.log("this is About page");
+//# sourceMappingURL=index-BWDW8IAO.js.map
---
+++ task-name/assets/index-q6LbEYNN.js
@@ -0,0 +1,2 @@
+import"./modulepreload-polyfill-B5Qt9EMX.js";console.log("this is Home page");
+//# sourceMappingURL=index-q6LbEYNN.js.map
