1832bf0693106aae6a869e37e11ef2f3eee8a200
Sergei Goncharov <blist.zheleza@gmail.com>
2024-11-21 15:52:17 +0700

    test

--- task-name/src/main.js
+++
@@ -1,1 +0,0 @@
-console.log('this is Home page');
--- task-name/vite.config.js
+++ task-name/vite.config.js
@@ -1,15 +1,23 @@
-import {defineConfig} from "vite";
-import {sync} from "glob";
+import { defineConfig } from 'vite';
+import { sync } from 'glob';
 
 export default defineConfig({
-  root: "./src",
-  base: "/test-gh-pages/task-name",
+  root: './src',
+  base: '/serezha-goncharov-JSFE2024Q4/christmas-shop/',
   build: {
     sourcemap: true,
     emptyOutDir: true,
     outDir: '../dist',
     rollupOptions: {
-      input: sync("./src/**/*.html".replace(/\\/g, "/")),
-    }
-  }
+      input: sync('./src/**/*.html'.replace(/\\/g, '/')),
+    },
+  },
+  css: {
+    devSourcemap: true,
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler',
+      },
+    },
+  },
 });
